0461b5af08ff434cd394eedb7b6824cf5f4f6dc2
tjtrbnet <65527169+tjtrbnet@users.noreply.github.com>
2020-11-17 12:56:05 +1100

    Update linuxvm.tf

--- azure/linuxVM-Existing-RG-Vnet/linuxvm.tf
+++ azure/linuxVM-Existing-RG-Vnet/linuxvm.tf
@@ -65,7 +65,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
     
      admin_ssh_key {
     username   = "adminuser"
-    public_key = file("~/.ssh/id_rsa.pub")
+    public_key = file("~/.ssh/dev_rsa.pub")
   }
 
     os_disk  {
